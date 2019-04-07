b65d712219f2c505713e8ac0d8140b0d6f80a714
Tommy Truong <tommy@truong.rocks>
2019-04-07 16:52:57 -0400

    Added FlexLayout
    Added flexlayout to application.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {StoreModule} from '@ngrx/store';
 import {reducers, effects} from './store';
 import {EffectsModule} from '@ngrx/effects';
 import {AdminModule} from './modules/admin/admin.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import {AdminModule} from './modules/admin/admin.module';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     StoreModule.forRoot(reducers),
-    EffectsModule.forRoot(effects)
+    EffectsModule.forRoot(effects),
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
